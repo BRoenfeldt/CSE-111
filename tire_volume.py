8a2ff1ca3be0781bb1e8ea1169de4071556e4b90
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-04-30 13:44:43 -0600

    tire prove

--- tire_volume.py
+++ tire_volume.py
@@ -4,11 +4,17 @@ CSE111 -  12:45
 """
 
 import math
+from datetime import datetime
 
+def computeVolume():
+    width = float(input("What is the width of the tire in mm? "))
+    ratio = float(input("What is the aspect ratio of the tire? "))
+    diam = float(input("What is the diameter of the tire in inches? "))
+    vol = (math.pi*(width*width)*ratio*(width*ratio+2540*diam)/10000000000)
+    #print(f"The approximate volume of the tires is {vol:.2f} liters")
 
+def  addToVolText():
+    with open("tireVolume.txt", "a") as file:
+        file.write(f"{datetime.now()}\n")
 
-width = float(input("What is the width of the tire in mm? "))
-ratio = float(input("What is the aspect ratio of the tire? "))
-diam = float(input("What is the diameter of the tire in inches? "))
-vol = (math.pi*(width*width)*ratio*(width*ratio+2540*diam)/10000000000)
-print(f"The approximate volume of the tires is {vol:.2f} liters")
+addToVolText()
