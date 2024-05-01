f28be6845b08ad9b5fbe306df06470eb270b5a44
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-01 12:26:41 -0600

    tire volume

--- tire_volume.py
+++ tire_volume.py
@@ -7,14 +7,19 @@ import math
 from datetime import datetime
 
 def computeVolume():
-    width = float(input("What is the width of the tire in mm? "))
-    ratio = float(input("What is the aspect ratio of the tire? "))
-    diam = float(input("What is the diameter of the tire in inches? "))
+    width = int(input("What is the width of the tire in mm? "))
+    ratio = int(input("What is the aspect ratio of the tire? "))
+    diam = int(input("What is the diameter of the tire in inches? "))
     vol = (math.pi*(width*width)*ratio*(width*ratio+2540*diam)/10000000000)
+    vol = round(vol, 2)
+    return(width, ratio, diam, vol)
     #print(f"The approximate volume of the tires is {vol:.2f} liters")
 
 def  addToVolText():
     with open("tireVolume.txt", "a") as file:
-        file.write(f"{datetime.now()}\n")
+        currentDateTime = datetime.now(tz=None)
+        currentDateTime = currentDateTime.strftime("%m/%d/%Y")
+        file.write(f"Date: {currentDateTime}, Tire Information: {computeVolume()}\n")
+        #print(currentDateTime)
 
 addToVolText()
