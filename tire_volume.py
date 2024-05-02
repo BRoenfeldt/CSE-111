17aa9e5c9d4aed655f3787380361af8809fbbd68
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-02 13:16:26 -0600

    week2 prove

--- tire_volume.py
+++ tire_volume.py
@@ -7,19 +7,43 @@ import math
 from datetime import datetime
 
 def computeVolume():
+    """
+    Gets user input from terminal and calculates the volume of the tire
+
+    Args:
+        None
+
+    Returns:
+        3 integers: width, ratio, diameter
+        1 float: volume
+    """
     width = int(input("What is the width of the tire in mm? "))
     ratio = int(input("What is the aspect ratio of the tire? "))
     diam = int(input("What is the diameter of the tire in inches? "))
     vol = (math.pi*(width*width)*ratio*(width*ratio+2540*diam)/10000000000)
     vol = round(vol, 2)
     return(width, ratio, diam, vol)
-    #print(f"The approximate volume of the tires is {vol:.2f} liters")
+    #print(f"The approximate volume of the tires is {vol:.2f} liters") used for debugging
 
 def  addToVolText():
+    """
+    Adds the date and tire information to a text file named tireVolume.txt
+    Calls computeVolume() to get the tire information
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     with open("tireVolume.txt", "a") as file:
         currentDateTime = datetime.now(tz=None)
         currentDateTime = currentDateTime.strftime("%m/%d/%Y")
         file.write(f"Date: {currentDateTime}, Tire Information: {computeVolume()}\n")
-        #print(currentDateTime)
+        #print(currentDateTime), used for debugging
 
-addToVolText()
+def main():
+    addToVolText()
+
+if __name__ == "__main__":
+    main()
