9c44e0c352371d79b7e783d73ffa699fd123b8b8
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-21 13:32:47 -0600

    week5

---
+++ test_names.py
@@ -0,0 +1,31 @@
+"""
+Braden Roenfeldt
+Brother Keers
+CSE 111 12:45
+"""
+
+
+from names import make_full_name, \
+    extract_family_name, extract_given_name
+import pytest
+
+def test_make_full_name():
+    assert make_full_name("Sally", "Brown") == "Brown; Sally"
+    assert make_full_name("Braden", "Roenfeldt") == "Roenfeldt; Braden"
+    assert make_full_name("Chris", "Keers") == "Keers; Chris"
+
+
+def test_extract_family_name():
+    assert extract_family_name("Brown; Sally") == "Brown"
+    assert extract_family_name("Roenfeldt; Braden") == "Roenfeldt"
+    assert extract_family_name("Keers; Chris") == "Keers"
+
+def test_extract_given_name():
+    assert extract_given_name("Brown; Sally") == "Sally"
+    assert extract_given_name("Roenfeldt; Braden") == "Braden"
+    assert extract_given_name("Keers; Chris") == "Chris"
+
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
