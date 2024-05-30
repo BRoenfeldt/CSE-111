39d3a19d9a43226252db0faf0a7de29f137b34f5
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-05-30 13:46:14 -0600

    week 6

--- esteem.py
+++ esteem.py
@@ -4,92 +4,95 @@ Brother Keers
 12:45
 """
 
-questionCounter = 1
-userInput = ""
 
 def main():
 
-
+    #prints the introduction to the program
     print(f"This program is an implementation of the Rosenberg Self-Esteem Scale. This program will show you ten statements that you could possibly apply to yourself. Please rate how much you agree with each of the statements by responding with one of these four letters:\nD means you strongly disagree with the statement.\nd means you disagree with the statement.\na means you agree with the statement.\nA means you strongly agree with the statement.")
-    while userInput == "A" or "a" or "d" or "D":
-        if questionCounter == 1:
-            userInputCheck(question1(), questionCounter)
-            print(userInput)
-        elif questionCounter == 2:
-            question2()
-        elif questionCounter == 3:
-            question3()
-        elif questionCounter == 4:
-            question4()
-        elif questionCounter == 5:
-            question5()
-        elif questionCounter == 6:
-            question6()
-        elif questionCounter == 7:
-            question7()
-        elif questionCounter == 8:
-            question8()
-        elif questionCounter == 9:
-            question9()
-        elif questionCounter == 10:
-            question10()
-        questionCounter += 1
-        
+
+    #initialize score as 0
+    score = 0
+    #Set each questsion as a variable so it can be passed to the question functions
+    q1 = "  1. I feel that I am a person of worth, at least on an equal plane with others."
+    q2 = "  2. I feel that I have a number of good qualities."
+    q3 = "  3. All in all, I am inclined to feel that I am a failure."
+    q4 = "  4. I am able to do things as well as most other people."
+    q5 = "  5. I feel I do not have much to be proud of."
+    q6 = "  6. I take a positive attitude toward myself."
+    q7 = "  7. On the whole, I am satisfied with myself."
+    q8 = "  8. I wish I could have more respect for myself."
+    q9 = "  9. I certainly feel useless at times."
+    q10 = " 10. At times I think I am no good at all."
+
+    #loops through questions 1-5 calling the  function to sent the questions to the user and get the answer then calling the getScore function to get the score of the answer
+    for question in [q1, q2, q3, q4, q5]:
+        answer = questions1_10(question)
+        score += getScorePos(answer)
+        #loops through questions 6-10 calling the  function to sent the questions to the user and get the answer then calling the getScore function to get the score of the answer
+    for question in [q6, q7, q8, q9, q10]:
+        answer = questions1_10(question)
+        score += getScoreNeg(answer)
+    #prints the score and the interpretation of the score
+    print(f"\nYour score is {score}.\nA score below 15 may indicate problematic low self-esteem.")
 
 
-def  userInputCheck(Input, questionCounter):
-    if Input != "A" or "a" or "d" or "D":
-        print("Invalid input. Please enter A, a, D, or d.")
-        questionCounter -= 1
+def questions1_10(question):
+    """
+    Function to ask questions 1-5
+    Parameters:
+        question: string to be asked to the user
+    Returns: 
+        answer: string of the user's response
+    """
+    #asks the user the question and gets the answer to return the answer
+    answer = input(f"{question} Answer (D, d, a, A): ")
+    #print(answer)
+    return answer
 
-def question1():
-    userInput = input(f"I feel that I am a person of worth, at least on an equal plane with others. ")
-    return (userInput)
+def getScorePos(answer):
+    """
+    Function to get the score of the user's response for questions 1-5
+    Parameters:
+        answer: string of the user's response
+    Returns: 
+        score: int of the user's score
+    """
+    #if the answer is D the score is 0
+    if answer == "D":
+        score = 0
+    #if the answer is d the score is 1
+    elif answer == "d":
+        score = 1
+    #if the answer is a the score is 2
+    elif answer == "a":
+        score = 2
+    #if the answer is A the score is 3
+    elif answer == "A":
+        score = 3
+    return score
 
-def question2():
-    userQ2 = input(f"I feel that I have a number of good qualities. ")
-
-def question3():
-    userQ3 = input(f"All in all, I am inclined to feel that I am a failure. ")
-
-def question4():
-    userQ4 = input(f"I am able to do things as well as most other people. ")
-
-def question5():
-    userQ5 = input(f"I feel I do not have much to be proud of. ")
-
-def question6():
-    userQ6 = input(f"I take a positive attitude toward myself. ")
-
-def question7():
-    userQ7 = input(f"On the whole, I am satisfied with myself. ")
-
-def question8():
-    userQ8 = input(f"I wish I could have more respect for myself. ")
-
-def question9():
-    userQ9 = input(f"I certainly feel useless at times. ")
-
-def question10():
-    userQ10 = input(f"At times I think I am no good at all. ")
-
-
+def getScoreNeg(answer):
+    """
+    Function to get the score of the user's response for questions 6-10
+    Parameters:
+        answer: string of the user's response
+    Returns: 
+        score: int of the user's score
+    """
+    #if the answer is D the score is 3
+    if answer == "D":
+        score = 3
+    #if the answer is d the score is 2
+    elif answer == "d":
+        score = 2
+    #if the answer is a the score is 1
+    elif answer == "a":
+        score = 1
+    #if the answer is A the score is 0
+    elif answer == "A":
+        score = 0
+    return score
 
 #runs main function
 if __name__ == "__main__":
     main()
-
-
-"""
-score = 0
-q1 = "how are you"
-q2 = "i hate you"
-
-def askQuestion(question):
-    answer = input(f" on a scale of 1-3 {question}?")
-    return answer
-
-x = askQuestion(q1)
-score += x
-x  = askQuestion(q2)
-score += x
--- water_flow.py
+++ water_flow.py
@@ -28,5 +28,83 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
     :param fluid_velocity: velocity of the fluid
     :return: pressure loss
     """
-    pressure_loss = ((-friction_factor) * pipe_length * 998.2 * (fluid_velocity**2)) / (2000 * pipe_diameter)
+    pressure_loss = -(friction_factor * pipe_length * 998.2 * (fluid_velocity**2)) / (2000 * pipe_diameter)
     return pressure_loss
+
+def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
+    """
+    Calculates the pressure loss in the fittings.
+    :param fluid_velocity: velocity of the fluid
+    :param quantity_fittings: quantity of fittings
+    :return: pressure loss
+    """
+    pressure_loss = -0.04 * quantity_fittings * 998.2 * (fluid_velocity**2) / 2000
+    return pressure_loss
+
+def reynolds_number(hydraulic_diameter, fluid_velocity):
+    """
+    Calculates the Reynolds number.
+    :param hydraulic_diameter: hydraulic diameter
+    :param fluid_velocity: velocity of the fluid
+    :return: Reynolds number
+    """
+    reynolds_number = 998.2 * hydraulic_diameter * fluid_velocity / 0.0010016
+    return reynolds_number
+
+def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
+    """
+    Calculates the pressure loss in the pipe reduction.
+    :param larger_diameter: diameter of the larger pipe
+    :param fluid_velocity: velocity of the fluid
+    :param reynolds_number: Reynolds number
+    :param smaller_diameter: diameter of the smaller pipe
+    :return: pressure loss
+    """
+    k = (0.1 + 50/reynolds_number) * (((larger_diameter/smaller_diameter)**4) -1)
+    pressure_loss = -k * 998.2 * (fluid_velocity**2) / 2000
+    return pressure_loss
+
+    PVC_SCHED80_INNER_DIAMETER = 0.28687 # (meters)  11.294 inches
+PVC_SCHED80_FRICTION_FACTOR = 0.013  # (unitless)
+SUPPLY_VELOCITY = 1.65               # (meters / second)
+
+HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
+HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
+HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
+
+
+def main():
+    tower_height = float(input("Height of water tower (meters): "))
+    tank_height = float(input("Height of water tank walls (meters): "))
+    length1 = float(input("Length of supply pipe from tank to lot (meters): "))
+    quantity_angles = int(input("Number of 90° angles in supply pipe: "))
+    length2 = float(input("Length of pipe from supply to house (meters): "))
+
+    water_height = water_column_height(tower_height, tank_height)
+    pressure = pressure_gain_from_water_height(water_height)
+
+    diameter = PVC_SCHED80_INNER_DIAMETER
+    friction = PVC_SCHED80_FRICTION_FACTOR
+    velocity = SUPPLY_VELOCITY
+    reynolds = reynolds_number(diameter, velocity)
+    loss = pressure_loss_from_pipe(diameter, length1, friction, velocity)
+    pressure += loss
+
+    loss = pressure_loss_from_fittings(velocity, quantity_angles)
+    pressure += loss
+
+    loss = pressure_loss_from_pipe_reduction(diameter,
+            velocity, reynolds, HDPE_SDR11_INNER_DIAMETER)
+    pressure += loss
+
+    diameter = HDPE_SDR11_INNER_DIAMETER
+    friction = HDPE_SDR11_FRICTION_FACTOR
+    velocity = HOUSEHOLD_VELOCITY
+    loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
+    pressure += loss
+
+    print(f"Pressure at house: {pressure:.1f} kilopascals")
+
+
+if __name__ == "__main__":
+    main()
