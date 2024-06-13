1bf04e5eeb39a4f1a4c0cfe34980d6dcf9ab0d50
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-06-13 13:12:58 -0600

    week8

--- students/students.py
+++ students/students.py
@@ -4,7 +4,24 @@ Brother Keers
 12:45
 """
 
-import csv
+
+def main():
+    students = read_dictionary("students.csv")
+    userInput = int(input("What is the I-Number you are looking for? "))
+    length = len(students)
+    counter = 0
+    for key in students:
+        counter += 1    #counter to keep track of how many times the loop has run      
+        if userInput == int(key):
+            print(students[key])
+            break   #breaks the loop if the number is found
+        elif counter == length:     #checks if the counter is equal to the length of the dictionary to determine if the number was not found
+            print("I-Number not found")
+    #print(counter)
+    #print(length)
+            
+
+
 def read_dictionary(filename):
     """Read the contents of a CSV file into a
     dictionary and return the dictionary.
@@ -14,12 +31,25 @@ def read_dictionary(filename):
     Return: a dictionary that contains
         the contents of the CSV file.
     """
+    dictionary = {}
+    with open(filename, "r") as file:
+        students = {}
+        skip_header = 1
+        for line in file:
+            if skip_header > 0:
+                skip_header -= 1
+                continue
+            parts = line.split(",")
+            students[parts[0]] = parts[1].strip()
+            number = parts[0]
+            name = parts[1]
+        print(students)
+    return(students)
 
 
-
-
+"""
 def read_dictionary(filename, key_column_index):
-    """Read the contents of a CSV file into a compound
+    Read the contents of a CSV file into a compound
     dictionary and return the dictionary.
 
     Parameters
@@ -28,8 +58,8 @@ def read_dictionary(filename, key_column_index):
             to use as the keys in the dictionary.
     Return: a compound dictionary that contains
         the contents of the CSV file.
-    """
-
+    
+"""
 
 
 
