1c846bbc3b1832c0748e1ddf2d45d4326810b72f
UnwilledMars <bradenroenfeldt35@gmail.com>
2024-07-13 19:27:04 -0600

    wewk12

--- finalproject/file_name_change_test.py
+++ finalproject/file_name_change_test.py
@@ -1,6 +1,9 @@
 from file_name_change import directory_fix, change_single_file_name
+from file_name_change import *
+
 from datetime import datetime
 import pytest
+
 
 current_time = datetime.now(tz=None)
 current_time = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
@@ -13,7 +16,9 @@ def test_change_single_file_name():
     #test that the file name is changed correctly and raises an error if error occurs
     pytest.raises(FileNotFoundError, change_single_file_name,"./testingfolder/testnameold.txt", "./testfolder/testnameold.txt")
 
-
+def test_grab_inputs():
+    #test that the inputs are grabbed correctly
+    assert grab_inputs("testnameold.txt", "testnamenew.txt", "testfolder") == ("testnameold.txt", "testnamenew.txt", "testfolder")
 
 
 
